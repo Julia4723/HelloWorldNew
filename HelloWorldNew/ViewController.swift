e1472a6374e3d70146ce3d92abaedf510b81d8ac
My <uk472363@yanex.ru>
2024-02-25 23:43:31 +0300

    edit 25.02

--- HelloWorldNew/ViewController.swift
+++ HelloWorldNew/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var signalGreen: UIView!
+    @IBOutlet var signalYellow: UIView!
+    @IBOutlet var signalRed: UIView!
+    
+    @IBOutlet var startChangeColorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        signalRed.layer.cornerRadius = 50
+        signalYellow.layer.cornerRadius = 50
+        signalGreen.layer.cornerRadius = 50
+
+        
+        
+        startChangeColorButton.layer.cornerRadius = 12
     }
+    
+    
+    
+    @IBAction func changeColorButton(_ sender: UIButton) {
+
+        var color = signalRed.alpha
+        color = signalYellow.alpha
+        color = signalGreen.alpha
+        
+
+        
+        switch color {
+        case signalRed.alpha :
+            sender.setTitle("Next", for: .normal)
+            signalRed.alpha = 1
+            
+            
+        case signalYellow.alpha :
+            sender.setTitle("Next", for: .normal)
+            signalRed.alpha = 0.3
+            signalYellow.alpha = 1
+            
+            
+        case signalGreen.alpha :
+            signalRed.alpha = 0.3
+            signalYellow.alpha = 0.3
+            signalGreen.alpha = 1
+            sender.setTitle("Next", for: .normal)
+            
+        default:
+            sender.setTitle("Start", for: .normal)
+        }
 
 
+    }
+    
 }
 
