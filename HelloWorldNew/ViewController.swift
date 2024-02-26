b1c7326e240eaa4d9fa5aef1f5619cbef7bab3e2
My <uk472363@yanex.ru>
2024-02-26 22:20:32 +0300

    new edd

--- HelloWorldNew/ViewController.swift
+++ HelloWorldNew/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
     @IBOutlet var signalGreen: UIView!
     @IBOutlet var signalYellow: UIView!
     @IBOutlet var signalRed: UIView!
@@ -20,46 +20,52 @@ final class ViewController: UIViewController {
         signalRed.layer.cornerRadius = 50
         signalYellow.layer.cornerRadius = 50
         signalGreen.layer.cornerRadius = 50
-
-        
+        signalRed.alpha = 0.3
+        signalYellow.alpha = 0.3
+        signalGreen.alpha = 0.3
         
         startChangeColorButton.layer.cornerRadius = 12
     }
     
     
-    
-    @IBAction func changeColorButton(_ sender: UIButton) {
-
+    @IBAction func changeColorSignal(_ sender: UIButton) {
         var color = signalRed.alpha
         color = signalYellow.alpha
         color = signalGreen.alpha
         
-
-        
         switch color {
-        case signalRed.alpha :
+        case signalRed.alpha:
             sender.setTitle("Next", for: .normal)
             signalRed.alpha = 1
-            
-            
-        case signalYellow.alpha :
-            sender.setTitle("Next", for: .normal)
+            signalYellow.alpha = 0.3
+            signalGreen.alpha = 0.3
+        case signalYellow.alpha:
             signalRed.alpha = 0.3
             signalYellow.alpha = 1
-            
-            
-        case signalGreen.alpha :
+        case signalGreen.alpha:
             signalRed.alpha = 0.3
-            signalYellow.alpha = 0.3
             signalGreen.alpha = 1
-            sender.setTitle("Next", for: .normal)
-            
         default:
-            sender.setTitle("Start", for: .normal)
+            signalGreen.alpha = 1
         }
-
-
+        
+        /*
+         if signalRed.alpha == 0.3 && signalYellow.alpha == 0.3 && signalGreen.alpha == 0.3 {
+         signalRed.alpha = 1
+         sender.setTitle("Next", for: .normal)
+         } else if signalRed.alpha == 1 {
+         signalRed.alpha = 0.3
+         signalYellow.alpha = 1
+         } else if signalYellow.alpha == 1 {
+         signalYellow.alpha = 0.3
+         signalGreen.alpha = 1
+         } else if signalGreen.alpha == 1 {
+         signalGreen.alpha = 0.3
+         }
+             
+         */
     }
     
+    
+    
 }
-
